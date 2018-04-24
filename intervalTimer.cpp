3bb613a46c4b6af4613366d2ae4dda2d5dcb24bd
trashviolent <wizzardnoise@gmail.com>
2018-04-24 14:38:39 -0400

    Update intervalTimer.cpp

--- intervalTimer.cpp
+++ intervalTimer.cpp
@@ -3,8 +3,8 @@
 
 int main(int argc, char *argv[])
 {
-   auto start_time = std::chrono::high_resolution_clock::now();
-   auto current_time = std::chrono::high_resolution_clock::now();
+   auto start_time = std::chrono::steady_clock::now();
+   auto current_time = std::chrono::steady_clock::now();
    int count = 0;
    std::chrono::microseconds duration(16667);
    std::chrono::duration<double> elapsed = current_time - start_time;
@@ -12,9 +12,9 @@ int main(int argc, char *argv[])
         if(elapsed >= duration) {
             std::cout << count << " intervals passed" << std::endl;
             ++count;
-            start_time = std::chrono::high_resolution_clock::now();
+            start_time = std::chrono::steady_clock::now();
         }
-        current_time = std::chrono::high_resolution_clock::now();
+        current_time = std::chrono::steady_clock::now();
         elapsed = current_time - start_time;
     }
    return 0;
