ad3ee3cf85640cc9f59d741c91629f109376c19b
trashviolent <wizzardnoise@gmail.com>
2018-04-24 14:07:10 -0400

    Update timer.cpp

--- timer.cpp
+++ timer.cpp
@@ -1,5 +1,3 @@
-//https://www.pluralsight.com/blog/software-development/how-to-measure-execution-time-intervals-in-c--
-
 #include <chrono>
 #include <iostream>
 
@@ -7,15 +5,16 @@ int main(int argc, char *argv[])
 {
    auto start_time = std::chrono::high_resolution_clock::now();
    auto current_time = std::chrono::high_resolution_clock::now();
-    std::chrono::seconds timer(10);
-    std::chrono::seconds time2(1);
-    while(std::chrono::duration_cast<std::chrono::seconds>(current_time - start_time) < timer) {
-        if(std::chrono::duration_cast<std::chrono::seconds>(current_time - start_time) == time2) {
+   std::chrono::duration<double> finish(10);
+   std::chrono::duration<double> count(0);
+   std::chrono::duration<double> elapsed = current_time - start_time;
+    while(count < finish) {
+        if(elapsed >= count) {
             std::cout << "Program has been running for " << std::chrono::duration_cast<std::chrono::seconds>(current_time - start_time).count() << " seconds" << std::endl;
-            auto time3 = time2 + std::chrono::seconds(1);
-            time2 = time3;
+            ++count;
         }
-        auto current_time = std::chrono::high_resolution_clock::now();
+        current_time = std::chrono::high_resolution_clock::now();
+        elapsed = current_time - start_time;
     }
    return 0;
 }
