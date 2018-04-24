c2595c1973cb2dd15d0edf8450581b7ca6d42472
trashviolent <wizzardnoise@gmail.com>
2018-04-23 20:16:43 -0400

    Update largeScene.cpp

--- largeScene.cpp
+++ largeScene.cpp
@@ -7,7 +7,13 @@ class LargeScene {
     size_t cellSizeY; //size of every cell of the scene on the y axis
     size_t cellNumX; //number of cells on the x axis
     size_t cellNumY; //number of cells on the y axis
+    size_t positionCellSizeX; //position inside a cell on the x axis
+    size_t positionCellSizeY; //position inside a cell on the y axis
+    size_t positionCellNumX; //position on the cell grid on the x axis
+    size_t positionCellNumY; //position on the cell grid on the y axis
   public:
-    LargeScene(size_t cellSizeX, size_t cellSizeY, size_t cellNumX, size_t cellNumY);
-    void getPosition(size_t cellSizeX, size_t cellSizeY, 
+    LargeScene(size_t cellSizeX, size_t cellSizeY, size_t cellNumX, size_t cellNumY, 
+               size_t positionCellSizeX, size_t positionCellSizeY, size_t positionCellNumX, 
+              size_t positionCellNumY);
+    
 };
