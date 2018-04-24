92cf46e63b96bc3673251788fd77c3887aae920b
trashviolent <wizzardnoise@gmail.com>
2018-04-24 14:23:13 -0400

    Update intervalTimer.cpp

--- intervalTimer.cpp
+++ intervalTimer.cpp
@@ -5,7 +5,6 @@ int main(int argc, char *argv[])
 {
    auto start_time = std::chrono::high_resolution_clock::now();
    auto current_time = std::chrono::high_resolution_clock::now();
-   std::chrono::seconds finish(10);
    int count = 0;
    std::chrono::microseconds duration(16667);
    std::chrono::duration<double> elapsed = current_time - start_time;
