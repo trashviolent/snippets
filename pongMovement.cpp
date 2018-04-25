e65bca8fae089548c16ebad90babf0433b3ca9a2
trashviolent <wizzardnoise@gmail.com>
2018-04-25 17:59:18 -0400

    Update pongMovement.cpp

--- pongMovement.cpp
+++ pongMovement.cpp
@@ -6,45 +6,64 @@ int main(int argc, char *argv[])
    auto start_time = std::chrono::steady_clock::now();
    auto current_time = std::chrono::steady_clock::now();
    int count = 0;
-   int moved =  0;
+   int y =  500;
    int moveRate = 1;
-   int moveDuration = 0;
+   int upMoveDuration = 0;
+   int downMoveDuration = 0;
    std::chrono::microseconds duration(16667);
    std::chrono::duration<double> elapsed = current_time - start_time;
     while(count <= 30) {
-        if(elapsed >= duration) {
-            ++moveDuration;
+        if(elapsed >= duration) { 
+            if(input == 'w') {
+               ++upMoveDuration;
+               if(downMoveDuration > 0)
+                  moveRate = 1;
+               downMoveDuration = 0;
+            }
+            else if(input == 's') {
+               ++downMoveDuration;
+               if(upMoveDuration > 0)
+                  moveRate = 1;
+               upMoveDuration = 0;
+            }
             switch(moveRate) {
                 case 1:
-                    ++moved;
-                    if(moveDuration > 2) {
+                    if(input == 'w')
+                     --y;
+                     if(input == 's')
+                        ++y;
+                    if(upmoveDuration > 2 || downMoveDuration > 2)
                         ++moveRate;
-                        moveDuration = 0;
-                    }
                     break;
                 case 2:
-                    moved += 5;
-                    if(moveDuration > 2) {
+                  if(input == 'w')
+                    y -= 5;
+                  if(input == 's')
+                     y += 5;
+                    if(upMoveDuration > 4 || downMoveDuration > 2)
                         ++moveRate;
-                        moveDuration = 0;
-                    }
                     break;
                 case 3:
-                    moved += 10;
-                    if(moveDuration > 2) {
+                  if(input == 'w')
+                     y -= 10;
+                  if(input == 's')
+                     y += 10;
+                    if(upMoveDuration > 6 || downMoveDuration > 6)
                         ++moveRate;
-                        moveDuration = 0;
-                    }
                     break;
                 case 4:
-                    moved += 15;
-                    if(moveDuration > 2) {
+                  if(input == 'w')
+                     y -= 15;
+                  if(input == 's')
+                     y += 15;
+                    if(upMoveDuration > 8 || downMoveDuration > 8)
                         ++moveRate;
-                        moveDuration = 0;
-                    }
                     break;
                 case 5:
-                    moved += 20;
+                  if(input == 'w')
+                     y -= 20;
+                  if(input == 's')
+                     y += 20;
                     break;
             }
             ++count;
