94ccf222f7da86898e9f0961bd35d7b2ffddc80b
trashviolent <wizzardnoise@gmail.com>
2018-04-25 19:47:43 -0400

    Update pongMovement.cpp

--- pongMovement.cpp
+++ pongMovement.cpp
@@ -6,44 +6,56 @@
 int main(int argc, char *argv[])
 {
    srand(time(NULL));
-   int startY = 0;
-   
-   auto start_time = std::chrono::steady_clock::now();
-   auto current_time = std::chrono::steady_clock::now();
-   int count = 0;
-   int moveRate = 0;
-   int upMoveDuration = 0;
-   int downMoveDuration = 0;
-   int y = 0;
-   bool downMove = true;
    std::chrono::microseconds duration(16667);
-   std::chrono::duration<double> elapsed = current_time - start_time;
-   if(elapsed >= duration) { 
-      if(input == 'w') {
-         if(downMove)
-            moveRate = 0;
-         downMove = false;
+   bool gameComplete = false;
+   while(!gameComplete) {
+      bool roundComplete = false;
+      while(!roundComplete) {
+         auto start_time = std::chrono::steady_clock::now();
+         auto current_time = std::chrono::steady_clock::now();
+         int startY = 0;
+         int startX = rand() % 1;
+         if(startX == 0)
+            startX *= -1;
+         int moveRate = 0;
+         int y = 0;
+         int moveDirection = 0;
+         std::chrono::duration<double> elapsed = current_time - start_time;
+         if(elapsed >= duration) { 
+            if(input == 'w') {
+               if(moveDirection == 0 || moveDirection == 1) {
+                  moveRate = 0;
+                  downMove = 2;
+               }
+            }
+            else if(input == 's') {
+               if(moveDiection == 0 || moveDirection == 2) {
+                  moveRate = 0;
+                  downMove = 1;
+               }
+            }
+            if(moveRate < 20)
+               ++moveRate;
+            if(input == '\0') {
+               moveRate = 0;
+               moveDirection = 0;
+            }
+            if(!downMove)
+               y -= moveRate;
+            else
+               y = moveRate;
+            ++count;
+            start_time = std::chrono::steady_clock::now();
+            sprite[0].setY(sprite[].getY() + y);
+            if(sprite[0].getY() <= 15)
+               sprite[0].setY(16);
+            if(sprite[0].getY() >= screenHeight)
+               sprite[0].setY(screenHeight - 1);
+            }
+            current_time = std::chrono::steady_clock::now();
+            elapsed = current_time - start_time;
+         }
       }
-      else if(input == 's') {
-         if(!downMove)
-            moveRate = 0;
-         downMove = true;
-      }
-      if(moveRate < 20)
-            ++moveRate;
-      if(!downMove)
-         y -= moveRate;
-      else
-         y = moveRate;
-      ++count;
-      start_time = std::chrono::steady_clock::now();
-      sprite[0].setY(sprite[].getY() + y);
-      if(sprite[0].getY() <= 15)
-         sprite[0].setY(16);
-      if(sprite[0].getY() >= screenHeight)
-         sprite[0].setY(screenHeight - 1);
    }
-   current_time = std::chrono::steady_clock::now();
-   elapsed = current_time - start_time;
    return 0;
 }
