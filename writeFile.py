142f943da89dd8aab3f14d93bc4baca57c75380e
trashviolent <wizzardnoise@gmail.com>
2018-04-28 21:58:18 -0400

    fixed image files to work properly

--- writeFile.py
+++ writeFile.py
@@ -6,14 +6,14 @@ file = Image.open(path)
 pixels = file.convert("RGBA")
 w = input("width: ")
 h = input("height: ")
-pixelArray = [[0 for y in range(h)] for x in range(w)]
-for one in range(w):
-	for two in range(h):
-		r,g,b,a = pixels.getpixel((one,two))
+pixelArray = [[0 for y in range(w)] for x in range(h)]
+for one in range(h):
+	for two in range(w):
+		r,g,b,a = pixels.getpixel((two,one))
 		pixelArray[one][two] = str(r) + " " + str(g) + " " + str(b) + " " + str(a) + " "
 file2 = open(output,"w")
-for a in range(w):
-	for b in range(h):
+for a in range(h):
+	for b in range(w):
 		file2.write(pixelArray[a][b])
 file2.close()
 subprocess.check_call(["editFile.exe", str(output), str(w), str(h)])
