f5d0b5385e6fe4ddc0f5ccd8a5d7023473d59c1b
trashviolent <wizzardnoise@gmail.com>
2018-04-30 17:43:21 -0400

    Update largeImage.cpp

--- largeImage.cpp
+++ largeImage.cpp
@@ -1,2 +1,15 @@
 #include "largeImage.h"
 #include <cstdlib>
+
+LargeImage::~LargeImage() {
+  for(size_t a = 0; a < pixelNum; ++a) {
+    delete[] pixels[a];
+    for(int b = 0; b < locationNum[a]; ++b) {
+      delete[] location[a][b];
+    }
+    delete[] location[a];
+  }
+  delete[] pixels;
+  delete[] location;
+  delete[] locationNum;
+}
