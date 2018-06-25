19fec76c0c88bdc4db1996b776cbb9c289e7fbfc
golightly <wizzardnoise@gmail.com>
2018-06-25 17:56:20 -0400

    Update memoryManagement2.cpp

--- memoryManagement2.cpp
+++ memoryManagement2.cpp
@@ -3,16 +3,17 @@
 
 void testFunc(uint8_t* &address) {
     std::cout << &address << std::endl;
-    uint8_t* test = address + 2;
+    uint8_t* test = address;
     test[0] = 5;
     test[1] = 7;
 }
 
 int main() {
     uint8_t* test = new uint8_t[10];
+    uint8_t* pointer = test + 2;
     test[0] = 1;
     test[1] = 2;
-    testFunc(test);
+    testFunc(pointer);
     for(int a = 0; a < 4; ++a) {
         std::cout << (int)test[a] << std::endl;
     }
