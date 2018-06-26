185434ff20633afd3589526b12d246f11bc281e6
golightly <wizzardnoise@gmail.com>
2018-06-26 16:34:46 -0400

    Update memoryManagement3.cpp

--- memoryManagement3.cpp
+++ memoryManagement3.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <cstddef>
 
+int main() {
+    uint8_t* memblock = new uint8_t[2];
+    uint8_t* address = memblock;
+    std::cout << (void*)memblock << std::endl;
+    std::cout << (void*)address << std::endl;
+    memblock[0] = 1;
+    memblock[1] = 2;
+    std::cout << (int)memblock[0] << std::endl;
+    std::cout << (int)memblock[1] << std::endl;
+    uint8_t* test = address;
+    test[0] = 3;
+    std::cout << (void*)test << std::endl;
+    test += 1;
+    test[0] = 4;
+    std::cout << (void*)test << std::endl;
+    std::cout << (int)memblock[0] << std::endl;
+    std::cout << (int)memblock[1] << std::endl;
+    delete[] memblock;
+    return 0;
+}
