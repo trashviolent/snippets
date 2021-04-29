430be760113d841cfe11e8a7f554179688ae1bee
golightly <18671493+golightly@users.noreply.github.com>
2021-04-29 19:45:05 -0400

    Update collisionDetection.cpp

--- collisionDetection.cpp
+++ collisionDetection.cpp
@@ -30,7 +30,7 @@ diagonalTileCollisionFunction() {
 		else
 			handle like a normal collision
 	}
-	else if(player is above and to the left of the obstacle to start with) {
+	else if(player is above and to the left of the obstacle + obstacle width to start with) {
 		if(obstacle slopes upwards left to right) {
 			if(actual collision occurs) {
 				if(player is moving straight right) {
@@ -70,7 +70,7 @@ diagonalTileCollisionFunction() {
 		else
 			handle like a normal collision
 	}
-	else if(player is below and to the left of the obstacle to start with) {
+	else if(player is below and to the left of the obstacle + obstacle width to start with) {
 		if(obstacle slopes downwards left to right) {
 			if(actual collision occurs) {
 				if(player is moving straight right) {
@@ -95,14 +95,47 @@ diagonalTileCollisionFunction() {
 }
 
 regularTileCollisionFunction() {
-	if(player is above and alongside the obstacle on the x axis) {
-		
+	if(player is above and to the left of the obstacle) {
+		if(the player's distance on the x axis to the obstacle is shorter than the distance on the y axis) {
+			movement to the right is valid
+			adjust y (collision occurs)
+		}
+		else if(the player's distance on the y axis to the obstacle is shorter than the distance on the x axis) {
+			movement downwards is valid
+			adjust x (collision occurs)
+		}
+		else if(the distances are equal) {
+			adjust x (collision occurs)
+			adjust y (collision occurs)
+		}
+	{
+	else if(player is above and alongside the obstacle on the x axis) {
+		if(player is moving down and to the right) {
+			movement to the left is valid
+			adjust y (collision occurs until player x is greater than obstacle x + width)
+		}
+		else if(player is moving straight down) {
+			adjust y (collision occurs)
+		}
+		else if(player is moving down and to the left) {
+			movement to the right is valid
+			adjust y (collision occurs until player x + width is less than obstacle width
+		}
+	}
+	else if(player is above and to the right of the obstacle) {
+
 	}
 	else if(player is to the right and alongside the obstacle on the y axis) {
 		
 	}
+	else if(player is under and to the right of the obstacle) {
+
+	}
 	else if(player is under and alongside the obstacle on the x axis) {
 		
+	}
+	else if(player is under and to the left of the obstacle) {
+
 	}
 	else if(player is to the left and alongside the obstacle on the y axis) {
 		
